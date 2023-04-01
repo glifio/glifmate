de75f900eb4a9da4c87be3583bbb42131f31d94d
Jon Schwartz <jpschwartz2@uwalumni.com>
2023-04-01 01:06:32 -0700

    :recycle: Refactor PreStakeTest to use BaseTest

--- test/PreStake.t.sol
+++ test/PreStake.t.sol
@@ -1,33 +1,11 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
 import "forge-std/Test.sol";
 
-import {WFIL} from "shim/WFIL.sol";
-import {PreStake} from "src/PreStake.sol";
-import {PoolToken} from "src/PoolToken.sol";
-import {IWFIL} from "src/Interfaces/IWFIL.sol";
-import {IPoolToken} from "src/Interfaces/IPoolToken.sol";
+import "./BaseTest.sol";
 
-contract PreStakeTest is Test {
-  uint256 constant DUST = 1_000;
-  uint256 constant MAX_FIL_AMOUNT = 2_000_000_000e18;
-
-  address owner = makeAddr("owner");
-  address staker = makeAddr("staker");
-
-  PreStake preStake;
-  PoolToken poolToken;
-  WFIL wFIL;
-
-  function setUp() public {
-    wFIL = new WFIL(owner);
-    poolToken = new PoolToken(owner);
-    preStake = new PreStake(owner, IWFIL(address(wFIL)), IPoolToken(address(poolToken)));
-    vm.startPrank(owner);
-    poolToken.setMinter(address(preStake));
-    vm.stopPrank();
-  }
+contract PreStakeTest is BaseTest {
 
   function testDepositFIL(uint256 depositAmount) public {
     depositAmount = bound(depositAmount, DUST, MAX_FIL_AMOUNT);
